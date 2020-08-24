73ddbd65a022bc8b61a02fce230fb189290e293d
macbre <maciej.brencz@gmail.com>
2020-08-24 21:54:01 +0200

    Implement /data.php endpoint

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 """
 airrohr-prometheus-exporter web-app
 """
+import logging
 from os import getenv
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+# set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('airrohr-prometheus-exporter')
 
 
 @app.route('/')
@@ -28,7 +33,17 @@ def data():
     """
     Collects incoming data from airrohs stations
     """
-    return '', 201
+    # X-Sensor: esp8266-12331981
+    sensor_id = request.headers.get('X-Sensor', 'unknown')
+
+    # You need to set the request content type to application/json
+    payload = request.get_json()
+
+    if sensor_id is None or payload is None:
+        return 'Bad request / no payload / no X-Sensor header set', 400
+
+    logger.info('Got metrics from "%s" sensor', sensor_id)
+    return 'Metrics received', 201
 
 
 if __name__ == "__main__":
