b50504fecd16e70c8fa41dba50fc1a35f38597ee
macbre <maciej.brencz@gmail.com>
2021-07-29 12:57:54 +0100

    server.sh: use exec to pass signals to Python
    Hence, this will now work:
    
    $ docker run macbre/airrohr
    [2021-07-29 11:57:32 +0000] [1] [INFO] Starting gunicorn 20.1.0
    [2021-07-29 11:57:32 +0000] [1] [INFO] Listening at: http://0.0.0.0:8888 (1)
    [2021-07-29 11:57:32 +0000] [1] [INFO] Using worker: gevent
    [2021-07-29 11:57:32 +0000] [8] [INFO] Booting worker with pid: 8
    ^C[2021-07-29 11:57:34 +0000] [1] [INFO] Handling signal: int
    [2021-07-29 11:57:34 +0000] [8] [INFO] Worker exiting (pid: 8)
    [2021-07-29 11:57:34 +0000] [1] [INFO] Shutting down: Master

--- server.sh
+++ server.sh
@@ -1,1 +1,1 @@
-gunicorn --bind 0.0.0.0:"${HTTP_PORT:-8888}" --threads=1 --worker-class=gevent --worker-connections=1000 --access-logfile - app:app
+exec gunicorn --bind 0.0.0.0:"${HTTP_PORT:-8888}" --threads=1 --worker-class=gevent --worker-connections=1000 --access-logfile - app:app
